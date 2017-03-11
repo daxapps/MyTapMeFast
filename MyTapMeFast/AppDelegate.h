fb3d83728712c60f5a2b2dee8ca3705e2083c13f
daxapps <daxapps777@gmail.com>
2017-03-11 12:50:38 -0600

    Initial Commit

---
+++ MyTapMeFast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTapMeFast
+//
+//  Created by Jason Crawford on 3/11/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTapMeFast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTapMeFast
+//
+//  Created by Jason Crawford on 3/11/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
